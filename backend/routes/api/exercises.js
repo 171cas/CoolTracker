885d94917fca1737eb40aa65f6039f0543cb798f
Cesar Solano <171cas@gmail.com>
2022-03-17 19:07:53 -0500

    validation done

--- backend/routes/api/exercises.js
+++ backend/routes/api/exercises.js
@@ -4,7 +4,7 @@ const asyncHandler = require('express-async-handler');
 
 const { restoreUser } = require('../../utils/auth');
 const { Exercise } = require('../../db/models');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
@@ -52,36 +52,83 @@ const validateCreate = [
         .notEmpty()
         .isLength({ min: 1, max: 30 })
         .withMessage('Please provide a valid Name.'),
-    check('distance')
-        .isInt({ min: 0, max: 10000 })
-        .withMessage('Please provide a valid distance [0-10000].'),
-    check('sets')
-        .isInt({ min: 1, max: 1000 })
-        .withMessage('Please provide a valid set [0-1000].'),
-    check('reps')
-        .isInt({ min: 1, max: 1000 })
-        .withMessage('Please provide valid reps [0-1000].'),
-    check('rest')
-        .isInt({ min: 0, max: 86400 })
-        .withMessage('Please provide valid reps [0-1000].'),
-    check('weight')
-        .isInt({ min: 1, max: 1500 })
-        .withMessage('Please provide valid reps [0-1000].'),
-    check('notes')
-        .isLength({ max: 30 })
-        .withMessage('Please provide a note with less than 500 characters.'),
-    check('completion_time')
-        .isInt({ max: 86400 })
-        .toInt()
-        .withMessage('Workout time should be less than a whole day.'),
+    // check('distance')
+    //     .isInt({ min: 0, max: 10000 })
+    //     .withMessage('Please provide a valid distance [0-10000].'),
+    // check('sets')
+    //     .isInt({ min: 1, max: 1000 })
+    //     .withMessage('Please provide a valid set [0-1000].'),
+    // check('reps')
+    //     .isInt({ min: 1, max: 1000 })
+    //     .withMessage('Please provide valid reps [0-1000].'),
+    // check('rest')
+    //     .isInt({ min: 0, max: 86400 })
+    //     .withMessage('Please provide valid rest [0-86400].'),
+    // check('weight')
+    //     .isInt({ min: 1, max: 1500 })
+    //     .withMessage('Please provide valid reps [0-1500].'),
+    // check('notes')
+    //     .isLength({ max: 30 })
+    //     .withMessage('Please provide a note with less than 500 characters.'),
+    // check('completion_time')
+    //     .isInt({ min: 1, max: 86400 })
+    //     .toInt()
+    //     .withMessage('Workout time should be less than a whole day.'),
     handleValidationErrors
 ];
+
+const secondValidate = [
+    body('distance').custom((value, { req }) => {
+        if (value && (value < 0 || value > 10000)) {
+            throw new Error('Please provide a valid distance [0-10000].');
+        }
+        return true;
+    }),
+    body('sets').custom((value, { req }) => {
+        if (value && (value < 1 || value > 1000)) {
+            throw new Error('Please provide a valid set [0-1000].');
+        }
+        return true;
+    }),
+    body('reps').custom((value, { req }) => {
+        if (value && (value < 1 || value > 1000)) {
+            throw new Error('Please provide valid reps [0-1000].');
+        }
+        return true;
+    }),
+    body('rest').custom((value, { req }) => {
+        if (value && (value < 0 || value > 86400)) {
+            throw new Error('Please provide valid rest [0-86400].');
+        }
+        return true;
+    }),
+    body('weight').custom((value, { req }) => {
+        if (value && (value < 0 || value > 5000)) {
+            throw new Error('Please provide valid weight [0-5000].');
+        }
+        return true;
+    }),
+    body('notes').custom((value, { req }) => {
+        if (value && (value.length > 1500)) {
+            throw new Error('Please provide a note with less than 500 characters.');
+        }
+        return true;
+    }),
+    body('completion_time').custom((value, { req }) => {
+        if (value && (value < 0 || value > 86400)) {
+            throw new Error('Workout time must be less than a whole day.');
+        }
+        return true;
+    }),
+    handleValidationErrors
+]
 
 // Create Exercise
 router.post(
     '/',
     restoreUser,
-    //validateCreate,
+    validateCreate,
+    secondValidate,
     asyncHandler(async (req, res) => {
         const {
             workout_id,
@@ -118,7 +165,8 @@ router.post(
 router.put(
     '/:id(\\d+)',
     restoreUser,
-    // validateCreate,
+    validateCreate,
+    secondValidate,
     asyncHandler(async (req, res) => {
         const exercise = await Exercise.findByPk(req.params.id)
         // if (req.user.id !== exercise.user_id) {
