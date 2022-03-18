9aed2e0d8e72e5e8e65d669a3682bbe7714d92b3
Cesar Solano <171cas@gmail.com>
2022-03-18 16:26:45 -0500

    done

--- frontend/src/components/WorkoutDetail/index.js
+++ frontend/src/components/WorkoutDetail/index.js
@@ -7,6 +7,9 @@ const WorkoutDetail = ({ propId }) => {
     let { workoutId } = useParams();
     if (propId) workoutId = propId;
 
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+
     const workouts = useSelector((state) => state.workouts)
     const workout = workouts[workoutId]
 
@@ -22,10 +25,31 @@ const WorkoutDetail = ({ propId }) => {
     // console.log('exList', exList)
     // console.log('exercisesWO', exercisesWO)
 
+    const handleClickDelete = async (e) => {
+        e.preventDefault();
+        await dispatch(deleteWorkout(+workoutId))
+    };
+
+    let reviewLinks;
+    if (sessionUser?.id === workout?.user_id) {
+        reviewLinks = (
+            <>
+                <button onClick={handleClickDelete}>Delete Workout</button>
+                <button>Edit Workout</button>
+            </>
+        );
+    } else {
+        reviewLinks = (
+            <>
+            </>
+        );
+    }
+
 
     return (
         <div className='singleWO'>
-            <p>Workout #{workout?.id}</p>
+            <p><NavLink to={`/workout/${workout?.id}`} >Workout #{workout?.id} </NavLink> </p>
+            {reviewLinks && reviewLinks}
             <p>Date #{workout?.date}</p>
             <p>Note #{workout?.notes}</p>
         </div>
