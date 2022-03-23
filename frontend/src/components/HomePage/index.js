61564f45ca62384087774d2083155f8b0b553a0d
Cesar Solano <171cas@gmail.com>
2022-03-23 11:07:03 -0500

    personal links

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,6 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 import './HomePage.css'
 
 const HomePage = () => {
+    const resumeLink = async (e) => {
+        e.preventDefault();
+        window.location.replace('https://www.linkedin.com/in/cesar-solano-320211230/');
+    };
+    const gitLink = async (e) => {
+        e.preventDefault();
+        window.location.replace('http://www.https://github.com/171cas/');
+    };
+    const linkeLink = async (e) => {
+        e.preventDefault();
+        window.location.replace('https://www.linkedin.com/in/cesar-solano-320211230/');
+    };
 
     return (
         <div className='containerHome'>
@@ -8,6 +22,13 @@ const HomePage = () => {
             <h2>Create a Workout!</h2>
             <h2>Add exercises to a Workout!</h2>
             <h2>Like Workouts!</h2>
+            <br /><br /><br />
+            <h2>Develop By:</h2>
+            <h3 className='myName' onClick={resumeLink}>Cesar Solano</h3>
+            <div>
+                <FontAwesomeIcon icon={faGithub} className='linkClass' onClick={gitLink} />
+                <FontAwesomeIcon icon={faLinkedin} className='linkClass' onClick={linkeLink} />
+            </div>
         </div>
     );
 
