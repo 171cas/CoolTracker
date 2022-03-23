0930746bc1d95aa9bf4da6749cb39647815114bc
Cesar Solano <171cas@gmail.com>
2022-03-23 11:14:19 -0500

    links style

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -5,15 +5,15 @@ import './HomePage.css'
 const HomePage = () => {
     const resumeLink = async (e) => {
         e.preventDefault();
-        window.location.replace('https://www.linkedin.com/in/cesar-solano-320211230/');
+        window.open('https://www.linkedin.com/in/cesar-solano-320211230/');
     };
     const gitLink = async (e) => {
         e.preventDefault();
-        window.location.replace('http://www.https://github.com/171cas/');
+        window.open('http://www.https://github.com/171cas/');
     };
     const linkeLink = async (e) => {
         e.preventDefault();
-        window.location.replace('https://www.linkedin.com/in/cesar-solano-320211230/');
+        window.open('https://www.linkedin.com/in/cesar-solano-320211230/');
     };
 
     return (
@@ -26,7 +26,7 @@ const HomePage = () => {
             <h2>Develop By:</h2>
             <h3 className='myName' onClick={resumeLink}>Cesar Solano</h3>
             <div>
-                <FontAwesomeIcon icon={faGithub} className='linkClass' onClick={gitLink} />
+                <FontAwesomeIcon icon={faGithub} className='gitClass' onClick={gitLink} />
                 <FontAwesomeIcon icon={faLinkedin} className='linkClass' onClick={linkeLink} />
             </div>
         </div>
